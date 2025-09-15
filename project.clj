3a985b9a414bbe244b7d1c00a28b7874f68a8084
Ivan Grishaev <ivan@grishaev.me>
2025-09-15 19:05:32 +0300

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def MIN_JAVA_VERSION "11")
 
-(defproject com.github.igrishaev/pg-bin "0.1.0"
+(defproject com.github.igrishaev/pg-bin "0.1.1-SNAPSHOT"
 
   :description
   "Parse binary Postgres COPY output"
